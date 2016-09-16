05656412501d60d83ed3353b79b94b08cd92f229
☆Z★ <myaetta@gmail.com>
2016-09-15 19:39:47 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function respond() {
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage(cool());
     this.res.end();
   } else {
     console.log("don't care");
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = postMessage();
 
   options = {
     hostname: 'api.groupme.com',
